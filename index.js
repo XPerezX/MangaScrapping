d13aa6494ad5037d72598e6de96cca7250f0974f
XPerezX <perez@startapp.one>
2021-04-17 14:54:33 -0300

    getting urls

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const puppeteer = require("puppeteer");
+const fs = require('fs');
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: false});
     const page = await browser.newPage();
     await page.goto("https://mangalivre.net/ler/mashle/online/293786/capitulo-58");
     
     //await page.click('div.page-next');
-    await page.evaluate(() => {
+    const getPages = await page.evaluate(() => {
 
     const MangaCap = [];
+
+    const ElementToNextPage =  document.querySelector("div.page-next");
 
     const initialandLastPages = [...document.querySelectorAll(".page-navigation em")].map(em => em.innerHTML);
     
@@ -23,12 +26,15 @@ const puppeteer = require("puppeteer");
 
         for(let count = firstPage; count < lastPage; count++) {
             MangaCap.push(document.querySelector(".manga-image img").src);
+            ElementToNextPage.click();
         }
- 
-        // const list = [...document.querySelectorAll(".manga-image img")].map(({src}) => MangaCap.push(src) );
+        
         console.log(MangaCap);
+        //const test_html = `<html><img src="https://i0.wp.com/static2cdn.mangalivre.com/firefox/-J1phfjdriPti3UgyAGr7Q/m6824745/9189/280576/293786/003.png"></html>`;
+        return MangaCap;
     });
-    //await page.pdf({path: 'manga.pdf'});
+
+    //await page.pdf({ path: "manga.pdf", format: "a4" });
   
-   // await browser.close();
+   //await browser.close();
   })();
